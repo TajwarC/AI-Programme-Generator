5dd814bc1b29f724a4efd0b1ea70a4c2a6608faf
TajwarC <tajwarc96@gmail.com>
2025-07-16 11:15:38 +0100

    Setting up App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import {ClerkProviderWithRoutes} from "./auth/ClerkProviderWithRoutes.jsx"
+import ClerkProviderWithRoutes from "frontend\src\auth\ClerkProviderWithRoutes.jsx"
 import {Routes, Route} from "react-router-dom"
+import {Layout} from "frontend\src\layout\Layout.jsx"
+import {ProgrammeGenerator} from "frontend\src\programme\ProgrammeGenerator.jsx";
+import {HistoryPanel} from "frontend\src\history\HistoryPanel.jsx";
+import {AuthenticationPage} from "frontend\src\auth\AuthenticationPage.jsx"
 import './App.css'
 
 function App() {
   return <ClerkProviderWithRoutes>
     <Routes>
-      
+      <Route path="/sign-in/*" element={<AuthenticationPage />} />
+      <Route path="/sign-up" element={<AuthenticationPage />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<ProgrammeGenerator />} />
+        <Route path="/" element={<HistoryPanel />} />
+      </Route>
     </Routes>
   </ClerkProviderWithRoutes>
 
