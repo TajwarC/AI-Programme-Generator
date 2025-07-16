0bc30ddf669299c395545ba12ffb6d7f67cb4038
TajwarC <tajwarc96@gmail.com>
2025-07-16 10:52:34 +0100

    Adding Frontend and Backend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {ClerkProviderWithRoutes} from "./auth/ClerkProviderWithRoutes.jsx"
+import {Routes, Route} from "react-router-dom"
+import './App.css'
+
+function App() {
+  return <ClerkProviderWithRoutes>
+    <Routes>
+      
+    </Routes>
+  </ClerkProviderWithRoutes>
+
+}
+
+export default App
